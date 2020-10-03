53fb2d54774c5e95ea8a3c60fded2748602a70f6
DanikKaragodin <51264803+DanikKaragodin@users.noreply.github.com>
2020-10-03 10:17:56 +0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,32 @@
+ #Первое задание
+print("Silence is golden")
+#Второе задание
+print("Введите два числа:")
+a= int(input())
+b=int(input())
+print(a, "+", b, "=", a+b)
+print(a, "*", b, "=", a*b)
+#Третее задание
+print("Введите три числа:")
+c= int(input())
+d=int(input())
+f=int(input())
+c*=2
+d-=3
+f*=f
+print(c, "+", d, "+", f, "=", c+d+f) 
+#Четвертое задание
+print('''
+AAAAA
+AAAAA
+AAAAA
+AAAAA
+AAAAA
+AAAAA
+AAAAA
+AAAAA
+    ''')
+
+
+
+
