22efc5a91ff63fc78248e157c02f7e7417484387
DanikKaragodin <51264803+DanikKaragodin@users.noreply.github.com>
2020-10-03 10:44:56 +0300

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,12 +1,18 @@
+import time
  #Первое задание
+print("Первое задание:")
 print("Silence is golden")
+time.sleep(1)
 #Второе задание
+print("Второе задание:")
 print("Введите два числа:")
 a= int(input())
 b=int(input())
 print(a, "+", b, "=", a+b)
 print(a, "*", b, "=", a*b)
+time.sleep(1)
 #Третее задание
+print("Третее задание:")
 print("Введите три числа:")
 c= int(input())
 d=int(input())
@@ -14,8 +20,10 @@ f=int(input())
 c*=2
 d-=3
 f*=f
-print(c, "+", d, "+", f, "=", c+d+f) 
+print(c, "+", d, "+", f, "=", c+d+f)
+time.sleep(1)
 #Четвертое задание
+print("Четвертое задание:")
 print('''
 AAAAA
 AAAAA
@@ -26,6 +34,8 @@ AAAAA
 AAAAA
 AAAAA
     ''')
+time.sleep(5)
+
 
 
 
