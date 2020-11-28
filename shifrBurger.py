b28d96290cca4715fb3fb6d977e8b31c95e25c22
DanikKaragodin <51264803+DanikKaragodin@users.noreply.github.com>
2020-11-28 13:02:46 +0300

    Add files via upload

---
+++ shifrBurger.py
@@ -0,0 +1,40 @@
+s=input("input string: ")
+s1=s[:int(len(s)/2)]
+print("first half: ",s1)
+s2=s[int(len(s)/2):]
+print("second half: ",s2)
+j=0
+i=0
+if(len(s)%2==0):
+    while i<int(len(s)/2):
+
+        if(i==len(s2)-1):
+            s1=s1+s2[int(len(s2)-1)]
+            break
+        s1=s1.replace(s1[j]+s1[j+1],s1[j]+s2[i]+s1[j+1])
+        j+=2
+        i+=1
+ #else:    
+#     while i<int(len(s)/2):
+
+#         if(i==len(s2)-2):
+#             s1=s1+s2[int(len(s2)-2)]+s2[int(len(s2)-1)]
+#             break
+#         s1=s1.replace(s1[j]+s1[j+1],s1[j]+s2[i]+s1[j+1])
+#         j+=2
+#         i+=1
+print("shivrovka: ",s1)
+
+#-------------------------------------------------
+
+s3=""
+s4=""
+i=0
+while i < len(s1):
+    if i%2==0:
+        s3=s3+s1[i]
+    else:
+        s4=s4+s1[i]
+    i+=1
+deshifr=s3+s4
+print(deshifr)
