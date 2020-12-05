2a96529a406099453f2ed3fe78b08126b63cb493
DanikKaragodin <51264803+DanikKaragodin@users.noreply.github.com>
2020-12-05 10:12:44 +0300

    Add files via upload

--- shifrBurger.py
+++ shifrBurger.py
@@ -1,28 +1,22 @@
 s=input("input string: ")
+if(len(s)%2!=0):
+    s=s+" "
 s1=s[:int(len(s)/2)]
 print("first half: ",s1)
 s2=s[int(len(s)/2):]
 print("second half: ",s2)
 j=0
 i=0
-if(len(s)%2==0):
-    while i<int(len(s)/2):
 
-        if(i==len(s2)-1):
-            s1=s1+s2[int(len(s2)-1)]
-            break
-        s1=s1.replace(s1[j]+s1[j+1],s1[j]+s2[i]+s1[j+1])
-        j+=2
-        i+=1
- #else:    
-#     while i<int(len(s)/2):
+while i<int(len(s)/2):
 
-#         if(i==len(s2)-2):
-#             s1=s1+s2[int(len(s2)-2)]+s2[int(len(s2)-1)]
-#             break
-#         s1=s1.replace(s1[j]+s1[j+1],s1[j]+s2[i]+s1[j+1])
-#         j+=2
-#         i+=1
+    if(i==len(s2)-1):
+        s1=s1+s2[int(len(s2)-1)]
+        break
+    s1=s1.replace(s1[j]+s1[j+1],s1[j]+s2[i]+s1[j+1])
+    j+=2
+    i+=1
+ 
 print("shivrovka: ",s1)
 
 #-------------------------------------------------
@@ -37,4 +31,4 @@ while i < len(s1):
         s4=s4+s1[i]
     i+=1
 deshifr=s3+s4
-print(deshifr)
+print("deshifrovka: ", deshifr)
